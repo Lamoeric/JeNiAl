3b6914c8ce0b23c457eef68dae69a7b75e952c7b
Lamoeric <135678856+Lamoeric@users.noreply.github.com>
2023-06-06 14:25:50 -0400

    Second commit

---
+++ public_html/app/billview/billview.js
@@ -0,0 +1,303 @@
+'use strict';
+
+angular.module('cpa_admin.billview', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/billview', {
+		templateUrl: 'billview/billview.html',
+		controller: 'billviewCtrl',
+		resolve: {
+			auth: function ($q, authenticationService) {
+				var userInfo = authenticationService.getUserInfo();
+				if (userInfo) {
+					if (userInfo.privileges.admin_access==true) {
+						return $q.when(userInfo);
+					} else {
+						return $q.reject({authenticated: true, validRights: false, newLocation:null});
+					}
+				} else {
+					return $q.reject({authenticated: false, newLocation: "/billview"});
+				}
+			}
+		}
+	})
+	.when('/billview/:billid', {
+		templateUrl: 'billview/billview.html',
+		controller: 'billviewCtrl',
+		resolve: {
+			auth: function ($q, authenticationService, $location) {
+				var userInfo = authenticationService.getUserInfo();
+				if (userInfo && userInfo.privileges.admin_access==true) {
+					return $q.when(userInfo);
+				} else {
+					return $q.reject({ authenticated: false });
+				}
+			}
+		}
+	});
+;
+}])
+
+.controller('billviewCtrl', ['$rootScope','$scope', '$http', '$uibModal', '$route', '$location', 'anycodesService', 'dialogService', 'listsService', 'authenticationService', 'translationService', 'billingService', function($rootScope, $scope, $http, $uibModal, $route, $location, anycodesService, dialogService, listsService, authenticationService, translationService, billingService) {
+
+	$scope.progName = "billView";
+	$scope.leftpanetemplatefullpath = "./billview/bill.template.html";
+	$scope.currentBill = null;
+	$scope.currentLanguage = authenticationService.getCurrentLanguage();
+	$scope.selectedBill = null;
+	$scope.newBill = null;
+	$scope.selectedLeftObj = null;
+	$scope.isFormPristine = true;
+	$scope.globalErrorMessage = [];
+	$scope.globalWarningMessage = [];
+	$scope.billid = $route.current.params.billid;
+
+	// For delay display
+	$scope.delay = 0;
+	$scope.minDuration = 0;
+	$scope.message = 'Please Wait...';
+	$scope.backdrop = true;
+	$scope.promise = null;
+
+	$scope.isDirty = function() {
+		if ($scope.detailsForm.$dirty) {
+			return true;
+		}
+		return false;
+	};
+
+	$scope.setDirty = function() {
+		$scope.detailsForm.$dirty = true;
+		$scope.isFormPristine = false;
+	};
+
+	$scope.setPristine = function() {
+		$scope.detailsForm.$setPristine();
+		$scope.isFormPristine = true;
+	};
+
+	$scope.getAllBills = function () {
+		$http({
+				method: 'post',
+				url: './billview/billview.php',
+				data: $.param({'language' : authenticationService.getCurrentLanguage(), 'type' : 'getAllBills' }),
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+		}).
+		success(function(data, status, headers, config) {
+			if (data.success) {
+				if (!angular.isUndefined(data.data)) {
+					$scope.leftobjs = data.data;
+				} else {
+					$scope.leftobjs = [];
+				}
+			} else {
+				if (!data.success) {
+					dialogService.displayFailure(data);
+				}
+			}
+		}).
+		error(function(data, status, headers, config) {
+			dialogService.displayFailure(data);
+		});
+	};
+
+	$scope.getBillDetails = function (billid) {
+		$scope.promise = $http({
+			method: 'post',
+			url: './billview/billview.php',
+			data: $.param({'id' : billid, 'language' : authenticationService.getCurrentLanguage(),'type' : 'getBillDetails' }),
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+		}).
+		success(function(data, status, headers, config) {
+			if (data.success && !angular.isUndefined(data.data)) {
+				$scope.currentBill = data.data[0];
+				billingService.calculateBillAmounts($scope.currentBill);
+			} else {
+				dialogService.displayFailure(data);
+				$scope.currentBill = null;
+			}
+		}).
+		error(function(data, status, headers, config) {
+			dialogService.displayFailure(data);
+		});
+	};
+
+	$scope.setCurrentInternal = function (bill, index) {
+		if (bill != null) {
+			$scope.selectedLeftObj = bill;
+			$scope.selectedBill = bill;
+			$scope.billid = null;
+			$scope.getBillDetails(bill.id);
+			$scope.setPristine();
+		} else {
+			$scope.selectedBill = null;
+			$scope.currentBill = null;
+			$scope.selectedLeftObj = null;
+		}
+	}
+
+	$scope.setCurrent = function (bill, index) {
+		if ($scope.isDirty()) {
+			dialogService.confirmDlg($scope.translationObj.main.msgformdirty, "YESNO", $scope.setCurrentInternal, null, bill, index);
+		} else {
+			$scope.setCurrentInternal(bill, index);
+			// if ($scope.billid) $scope.getBillDetails($scope.billid);
+			// $location.path('billview/');
+
+		}
+	};
+
+	$scope.deleteFromDB = function(confirmed) {
+		if ($scope.currentBill != null && !confirmed) {
+			dialogService.confirmDlg($scope.translationObj.main.msgdelete, "YESNO", $scope.deleteFromDB, null, true, null);
+		} else {
+			$http({
+				method: 'post',
+				url: './billview/billview.php',
+				data: $.param({'bill' : $scope.currentBill, 'type' : 'delete_bill' }),
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			}).
+			success(function(data, status, headers, config) {
+				if (data.success) {
+					$scope.leftobjs.splice($scope.leftobjs.indexOf($scope.selectedBill),1);
+					$scope.setCurrentInternal(null);
+					return true;
+				} else {
+					dialogService.displayFailure(data);
+					return false;
+				}
+			}).
+			error(function(data, status, headers, config) {
+				dialogService.displayFailure(data);
+				return false;
+			});
+		}
+	}
+
+	$scope.validateAllForms = function() {
+		var retVal = true;
+		$scope.globalErrorMessage = [];
+		$scope.globalWarningMessage = [];
+
+		if ($scope.detailsForm.$invalid) {
+				$scope.globalErrorMessage.push($scope.translationObj.main.msgerrallmandatory);
+		}
+		// if ($scope.currentMember.healthcareno == "") {
+		// 	$scope.globalWarningMessage.push($scope.translationObj.main.msgerrallmandatory);
+		// }
+
+		if ($scope.globalErrorMessage.length != 0) {
+			$scope.$apply();
+			$("#mainglobalerrormessage").fadeTo(2000, 500).slideUp(500, function(){$("#mainglobalerrormessage").hide();});
+			retVal = false;
+		}
+		if ($scope.globalWarningMessage.length != 0) {
+			$scope.$apply();
+			$("#mainglobalwarningmessage").fadeTo(2000, 500).slideUp(500, function(){$("#mainglobalwarningmessage").hide();});
+		}
+		return retVal;
+	}
+
+	$scope.saveToDB = function() {
+		if ($scope.currentBill == null || !$scope.isDirty()) {
+			dialogService.alertDlg("Nothing to save!", null);
+		} else {
+			if ($scope.validateAllForms() == false) return;
+			$http({
+				method: 'post',
+				url: './billview/billview.php',
+				data: $.param({'bill' : $scope.currentBill, 'type' : 'updateEntireBill' }),
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			}).
+			success(function(data, status, headers, config) {
+				if (data.success) {
+					// Select this bill to reset everything
+					$scope.setCurrentInternal($scope.selectedBill, null);
+					return true;
+				} else {
+					dialogService.displayFailure(data);
+					return false;
+				}
+			}).
+			error(function(data, status, headers, config) {
+				dialogService.displayFailure(data);
+				return false;
+			});
+		}
+	};
+
+	$scope.addBillToDB = function() {
+		$http({
+			method: 'post',
+			url: './billview/billview.php',
+			data: $.param({'bill' : $scope.newBill, 'type' : 'insert_bill' }),
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+		}).
+		success(function(data, status, headers, config) {
+			if (data.success) {
+				var newBill = {id:data.id, name:$scope.newBill.name};
+				$scope.leftobjs.push(newBill);
+				// We could sort the list....
+				$scope.setCurrentInternal(newBill);
+				return true;
+			} else {
+				dialogService.displayFailure(data);
+				return false;
+			}
+		}).
+		error(function(data, status, headers, config) {
+			dialogService.displayFailure(data);
+			return false;
+		});
+	};
+
+	// This is the function that creates the modal to create new bill
+	$scope.createNew = function (confirmed) {
+		if ($scope.isDirty() && !confirmed) {
+			dialogService.confirmDlg($scope.translationObj.main.msgformdirty, "YESNO", $scope.createNew, null, true, null);
+		} else {
+			$scope.newBill = {};
+			// Send the newBill to the modal form
+			$uibModal.open({
+					animation: false,
+					templateUrl: 'billview/newbill.template.html',
+					controller: 'childeditor.controller',
+					scope: $scope,
+					size: 'md',
+					backdrop: 'static',
+					resolve: {
+						newObj: function () {
+							return $scope.newBill;
+						}
+					}
+			})
+			.result.then(function(newBill) {
+					// User clicked OK and everything was valid.
+					$scope.newBill = newBill;
+					if ($scope.addBillToDB() == true) {
+					}
+			}, function() {
+					// User clicked CANCEL.
+//	        alert('canceled');
+			});
+		}
+	};
+
+	$rootScope.$on("billing.reload", function (event, current, previous, eventObj) {
+		$scope.setCurrentInternal($scope.selectedBill, null);
+	});
+
+	$rootScope.$on("authentication.language.changed", function (event, current, previous, eventObj) {
+		$scope.refreshAll();
+	});
+
+	$scope.refreshAll = function() {
+		$scope.getAllBills();
+		anycodesService.getAnyCodes($scope, $http, authenticationService.getCurrentLanguage(),'yesno', 'text', 'yesnos');
+		translationService.getTranslation($scope, 'billview', authenticationService.getCurrentLanguage());
+		if ($scope.billid) $scope.getBillDetails($scope.billid);
+		// $location.path('billview/');
+	}
+
+	$scope.refreshAll();
+}]);
