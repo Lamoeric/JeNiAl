1c9f6c6944032cb2f16715080e45cab134996c2e
Lamoeric <135678856+Lamoeric@users.noreply.github.com>
2023-11-10 18:06:47 -0500

    Added validations to preregistration
    Now validating at least one phone number
    Now validating the birthdate
    Now validating the postal code
    Now validating the email address format
    Now validating no @ in postal address

--- public_html/app/core/directives/formatbirthdate/formatbirthdate.directive.js
+++ public_html/app/core/directives/formatbirthdate/formatbirthdate.directive.js
@@ -1,42 +1,40 @@
 angular.module('core').directive('formatBirthdate', function() {
-    return {
-      require: 'ngModel',
-      link: function(scope, element, attrs, modelCtrl) {
-        var formatNumber = function(number) {
+	return {
+		require: 'ngModel',
+		link: function(scope, element, attrs, modelCtrl) {
+			var formatNumber = function(number) {
+				if (!number) { return ''; }
+				number = number.replace(/[^0-9]+/g, '');
+				number = String(number);
+				// Will return formattedNumber.
+				var formattedNumber = number;
+				// YYYY-MM-DD
+				var year = number.substring(0,4);
+				var month = number.substring(4, 6);
+				if (month && month/1 > 12) month = 12;
+				var day = number.substring(6, 8);
+				if (day && day/1 > 31) day = 31;
+				if (month) {
+					formattedNumber = year + "-" + month;
+				}
+				if (day) {
+					formattedNumber += ("-" + day);
+				}
+				if (formattedNumber !== number) {
+					modelCtrl.$setViewValue(formattedNumber);
+					modelCtrl.$render();
+				}
+				// if (year && month && day && formattedNumber.length == 10) {
+				// 	modelCtrl.$setValidity(true);
+				// } else {
+				// 	modelCtrl.$setValidity(false);
+				// }
+				// scope[attrs.ngModel];
+				return formattedNumber;
+			}
 
-		      if (!number) { return ''; }
-					number = number.replace(/[^0-9]+/g, '');
-		      number = String(number);
-
-		      // Will return formattedNumber.
-		      var formattedNumber = number;
-
-					// if the first character is '1', strip it out and add it back
-					// var c = (number[0] == '1') ? '1 ' : '';
-					// number = number[0] == '1' ? number.slice(1) : number;
-
-					// YYYY-MM-DD
-					var year = number.substring(0,4);
-					var month = number.substring(4, 6);
-          if (month && month/1 > 12) month = 12;
-					var day = number.substring(6, 8);
-          if (day && day/1 > 31) day = 31;
-
-					if (month) {
-						formattedNumber = year + "-" + month;
-					}
-					if (day) {
-						formattedNumber += ("-" + day);
-					}
-          if (formattedNumber !== number) {
-            modelCtrl.$setViewValue(formattedNumber);
-            modelCtrl.$render();
-          }
-					return formattedNumber;
-        }
-
-        modelCtrl.$parsers.push(formatNumber);
-        formatNumber(scope[attrs.ngModel]); // formatNumber initial value
-      }
+			modelCtrl.$parsers.push(formatNumber);
+			formatNumber(scope[attrs.ngModel]); // formatNumber initial value
+      	}
     };
-  });
+});
