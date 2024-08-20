87bb30c5f7b8626fcf54107e5b1fa59ee8563056
Eric Lamoureux <>
2024-08-20 08:51:22 -0400

    Error handling v1
    Added error handling service, added Exception factory, modified invalidRequest to capture invalid request name

---
+++ public_html/app/core/services/errorhandling/errorhandling.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Author : Eric Lamoureux
+*/
+require_once('../../../../../private/' . $_SERVER['HTTP_HOST'] . '/include/config.php');
+require_once('../../../../include/invalidrequest.php');
+
+if (isset($_POST['type']) && !empty(isset($_POST['type']))) {
+	$type = $_POST['type'];
+
+	switch ($type) {
+		case "insertLog":
+			insertLog($mysqli, $_POST['user'], $_POST['progname'], $_POST['message'], $_POST['stack'], $_POST['cause']);
+			break;
+		default:
+			invalidRequest($type);
+	}
+} else {
+	invalidRequest();
+};
+
+/**
+ * This function inserts an error log
+ */
+function insertLog($mysqli, $user, $progname, $message, $stack, $cause) {
+	try {
+		$data = array();
+		$user = isset($user) ? $user : '';
+		$progname = isset($progname) ? $progname : '';
+		$message = isset($message) ? $mysqli->real_escape_string($message) : '';
+		$stack = isset($stack) ? $mysqli->real_escape_string($stack) : '';
+		$cause = isset($cause) ? $mysqli->real_escape_string($cause) : '';
+
+		$query = "INSERT INTO cpa_logs (id, user, progname, `message`, stack, cause) 
+				  VALUES (NULL, '$user', '$progname', '$message', '$stack', '$cause')";
+		if (!$mysqli->query($query)) {
+			throw new Exception('insertLog - ' . $mysqli->sqlstate . ' - ' . $mysqli->error);
+		}
+		$data['success'] = true;
+		echo json_encode($data);
+		exit;
+	} catch (Exception $e) {
+		$data['success'] = false;
+		$data['message'] = $e->getMessage();
+		echo json_encode($data);
+		exit;
+	}
+};
--- public_html/include/invalidrequest.php
+++ public_html/include/invalidrequest.php
@@ -5,11 +5,14 @@ Author : Eric Lamoureux
 
 /**
  * This function simply returns an array indicating there was an invalid request
+ * 
+ * $type	Original $type that was passed nad that clearly is not supported. Could be null.
+ * 			Use this information to increase the amount of usefull data we log in the error log
  */
-function invalidRequest() {
+function invalidRequest($type=null) {
 	$data = array();
 	$data['success'] = false;
-	$data['message'] = "Invalid request.";
+	$data['message'] = "Invalid request." . (isset($type) ? " Request was for : " . $type : "");
 	echo json_encode($data);
 	exit;
 };
