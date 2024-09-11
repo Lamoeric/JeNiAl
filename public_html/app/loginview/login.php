4565fb0bae278499fbca64b63e654dd3b144d5ab
Eric Lamoureux <>
2024-09-10 23:37:34 -0400

    Added the version and DB datetime on the login dialog
    Added the version and DB datetime (now()) on the login dialog. Added the table cpa_version

---
+++ public_html/app/loginview/login.php
@@ -0,0 +1,46 @@
+ <?php
+/*
+Author : Eric Lamoureux
+*/
+require_once('../../../private/'. $_SERVER['HTTP_HOST'].'/include/config.php');
+require_once('../../include/nocache.php');
+require_once('../../include/invalidrequest.php');
+
+if (isset($_POST['type']) && !empty(isset($_POST['type']))) {
+	$type = $_POST['type'];
+
+	switch ($type) {
+		case "getVersionDetails":
+			getVersionDetails($mysqli);
+			break;
+		default:
+			invalidRequest($type);
+	}
+} else {
+	invalidRequest();
+};
+
+
+/**
+ * This function gets the details of the current version
+ */
+function getVersionDetails($mysqli) {
+	try{
+		$query = "SELECT *, now() dbnow FROM cpa_version";
+		$result = $mysqli->query($query);
+		$data = array();
+		while ($row = $result->fetch_assoc()) {
+			$data['data'][] = $row;
+		}
+		$data['success'] = true;
+		echo json_encode($data);exit;
+	}catch (Exception $e) {
+		$data = array();
+		$data['success'] = false;
+		$data['message'] = $e->getMessage();
+		echo json_encode($data);
+		exit;
+	}
+};
+
+?>
